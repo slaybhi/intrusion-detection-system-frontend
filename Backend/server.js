ef742a08441b6ee5934553ee877384af527b0ad1
Joann <joannpelza0@gmail.com>
2020-03-20 18:41:30 +0530

    Server connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,31 @@
-const express=require('express')
-const app=express()
-const port=8080;
-const bcrypt=require('bcrypt')
-//const session=require('express-session');
+var express=require("express")
+var app=express()
 
-var cors=require('cors');
-var bodyParser=require('body-parser');
-
+var cors=require("cors")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+var port=process.env.PORT || 8080
 
 
-var MongoClient=require('mongodb').MongoClient;             //Mongodb Connection
-var url="mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db){
-    if(err) throw err;
-    var dbo=db.db("admindb");
-    dbo.createCollection("user",function(err,res){
-        if(err) throw err;
-        console.log("Collection created");
-        db.close();
-    });
-});
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+    )
 
+const url="mongodb://localhost:27017/admindb"
+mongoose
+.connect(url,{useNewUrlParser:true})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err))
 
-app.use(cors());
-app.options('*',cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());                 //json converted to object
+var Users=require('./routes/Users')
 
+app.use('/',Users)
 
+app.listen(port, () => {
+    console.log("Server is running on port : " + port)
+})
