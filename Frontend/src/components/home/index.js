39d7631b461142126c4c89d0a3f60352df404d13
slaybhi <a.abhijith1adoor@gmail.com>
2020-03-21 22:30:28 +0530

    resolved the height issue

--- Frontend/src/components/home/index.js
+++ Frontend/src/components/home/index.js
@@ -4,12 +4,12 @@ import Signup from '../login/signup'
 class Home extends Component {
     render() {
         return (
-            <div  className='container-fluid'>
-                
-               <div className='grid-container row'>
-               <div className='lefthalf'></div>
+            <div  className='container-fluid background'>
+                 <div className='lefthalf'></div>
                 <div className='righthalf'></div>
                 <div className = 'bck-logo'></div>
+
+               <div className='grid-container row'>
                     <Signup className='col-md-6'/>
                     <Signin className='col-md-6'/>
                 </div>
